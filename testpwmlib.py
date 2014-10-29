3c58d09293ab00665ef3159208b5b4823cf02b42
Christoph Sarnowski <pixelbrei@gmail.com>
2014-10-29 16:15:27 +0100

    fix BOTH bugs that lead to password errors around char 20 (in default charset)

--- testpwmlib.py
+++ testpwmlib.py
@@ -14,6 +14,8 @@ class PWTest(unittest.TestCase):
     def test_generatepassword_20chars(self):
         res = self.pw.generatepassword('md5','asdf','passwordmaker.org'+''+'',False,1,20,self.pw.FULL_CHARSET,'','')
         self.assertEqual(res,'FRRHm)k+UyQiY~%Dj;h*')
-
+    def test_64chars(self):
+        res = self.pw.generatepassword('md5','asdf','passwordmaker.org'+''+'',False,1,64,self.pw.FULL_CHARSET,'','')
+        self.assertEqual(res,'FRRHm)k+UyQiY~%Dj;h*FV[{:5X@EN5krPbfUlY7BRv12Dl.QJ=-]pF}UyDtCZ9#')
 if __name__ == '__main__':
     unittest.main()
